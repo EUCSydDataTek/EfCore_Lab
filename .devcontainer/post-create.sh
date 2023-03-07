9dc7dea5b87dcaa0cb4db82f6242a64377129922
jako5457 <jako5457@gmail.com>
2023-03-07 12:46:51 +0100

    tilføjet post create script

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "---- Post create script ----"
+sudo apt-get update && sudo apt-get install python3 jupyter
+
+dotnet tool install -g Microsoft.dotnet-interactive
+
+dotnet interactive jupyter install
+
+dotnet restore && dotnet build
+echo "---- Post create script ----"
