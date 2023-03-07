239e7e4e9464053f564ddf4a29ee94ee603c4853
jako5457 <jako5457@gmail.com>
2023-03-07 13:16:11 +0100

    fixet installations Script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 echo "---- Post create script ----"
-sudo apt-get update && sudo apt-get install python3 jupyter
+sudo apt-get update && sudo apt-get install python3 jupyter -y
 
-dotnet tool install -g Microsoft.dotnet-interactive
+sudo dotnet tool install -g Microsoft.dotnet-interactive
 
-dotnet interactive jupyter install
+sudo dotnet interactive jupyter install
 
-dotnet restore && dotnet build
+sudo dotnet restore
 echo "---- Post create script ----"
