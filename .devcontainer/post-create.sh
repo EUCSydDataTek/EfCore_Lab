b50e5774729b23053debe934038742f1a3ac8126
Jakob spiel <jako5457@gmail.com>
2023-03-07 13:36:49 +0100

    Update devcontainer.json

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 echo "---- Post create script ----"
-sudo apt-get update && sudo apt-get install python3 jupyter -y
+sudo conda install jupyter
 
 sudo dotnet tool install -g Microsoft.dotnet-interactive
 
 sudo dotnet interactive jupyter install
 
 sudo dotnet restore
-echo "---- Post create script ----"
