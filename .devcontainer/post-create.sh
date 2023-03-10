21bcdadcfc9c047dbe309dc2cf4f79ca8a428c79
jako5457 <jako5457@gmail.com>
2023-03-10 10:58:23 +0100

    Fjernet devcontainer

--- .devcontainer/post-create.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-echo "---- Post create script ----"
-sudo conda install jupyter -y
-
-sudo dotnet tool install -g Microsoft.dotnet-interactive
-
-sudo dotnet interactive jupyter install
-
-sudo dotnet restore
