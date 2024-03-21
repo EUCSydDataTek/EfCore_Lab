d14bf285d4972ddd9fb9a086c830f938da91fa54
Jakob spiel <jako5457@gmail.com>
2024-03-21 08:12:20 +0000

    Opdateret til .Net8

--- .devcontainer/install-dotnet-interactive.sh
+++ .devcontainer/install-dotnet-interactive.sh
@@ -4,7 +4,7 @@
 apt-get update
 
 # Install .NET7
-apt-get install -y dotnet7 curl python3 jupyter git
+apt-get install -y dotnet8 curl python3 jupyter git
 
 dotnet new tool-manifest
 dotnet tool install --local Microsoft.dotnet-interactive
