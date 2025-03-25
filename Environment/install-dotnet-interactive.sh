53d0a9c77d8ebfc0527d02991b26fa33b3867e6e
Jakob spiel <jako5457@gmail.com>
2025-03-25 08:37:05 +0100

    Opdateret til dotnet 9

--- Environment/install-dotnet-interactive.sh
+++ Environment/install-dotnet-interactive.sh
@@ -4,7 +4,7 @@
 apt-get update
 
 # Install .NET7
-apt-get install -y dotnet9 curl python3 jupyter
+apt-get install -y dotnet-sdk-9.0 aspnetcore-runtime-9.0 dotnet-runtime-9.0 curl python3 jupyter
 
 dotnet new tool-manifest
 dotnet tool install --local Microsoft.dotnet-interactive
