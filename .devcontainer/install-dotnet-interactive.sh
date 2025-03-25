cd25626273c1f362778d7921db5cc7fc9065ef36
Jakob spiel <jako5457@gmail.com>
2025-03-25 08:31:20 +0100

    opdateret til NET9

--- .devcontainer/install-dotnet-interactive.sh
+++ .devcontainer/install-dotnet-interactive.sh
@@ -3,8 +3,8 @@
 # Update the package
 apt-get update
 
-# Install .NET7
-apt-get install -y dotnet8 curl python3 jupyter git
+# Install .NET9
+apt-get install -y dotnet9 curl python3 jupyter git
 
 dotnet new tool-manifest
 dotnet tool install --local Microsoft.dotnet-interactive
