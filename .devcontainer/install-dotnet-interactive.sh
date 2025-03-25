9057ddbed92b733ab2ef62a7ae77d80cd0e0f1a1
Jakob spiel <jako5457@gmail.com>
2025-03-25 08:54:22 +0100

    opdatering

--- .devcontainer/install-dotnet-interactive.sh
+++ .devcontainer/install-dotnet-interactive.sh
@@ -4,7 +4,7 @@
 apt-get update
 
 # Install .NET9
-apt-get install -y dotnet-sdk-9.0 aspnetcore-runtime-9.0 dotnet-runtime-9.0 curl python3 jupyter git
+apt-get install -y dotnet8 dotnet-sdk-9.0 aspnetcore-runtime-9.0 dotnet-runtime-9.0 curl python3 jupyter git
 
 dotnet new tool-manifest
 dotnet tool install --local Microsoft.dotnet-interactive
--- Environment/install-dotnet-interactive.sh
+++ Environment/install-dotnet-interactive.sh
@@ -4,7 +4,9 @@
 apt-get update
 
 # Install .NET7
-apt-get install -y dotnet-sdk-9.0 aspnetcore-runtime-9.0 dotnet-runtime-9.0 curl python3 jupyter
+apt-get install -y dotnet8 dotnet-sdk-9.0 aspnetcore-runtime-9.0 dotnet-runtime-9.0 curl python3 jupyter
+
+export DOTNET_ROOT=/usr/lib/dotnet
 
 dotnet new tool-manifest
 dotnet tool install --local Microsoft.dotnet-interactive
